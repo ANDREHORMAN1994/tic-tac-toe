6f213746671d4206ca6ce9d65048578b29bccac6
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-11-21 17:17:30 -0300

    :sparkles: criando eventos e adicionando imagens

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,66 @@
 const gameEl = document.querySelector('.container-game');
+const eventsEl = document.querySelector('.container-events');
+const checkEl = document.querySelector('.check-input');
 const ticTacList = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+const eventsList = [];
+let player = 'X';
+let checkValue = false;
 
-const handleClick = (value) => {
-  console.log(value);
+const createImage = (path) => {
+  const imgEl = document.createElement('img');
+  imgEl.classList.add('img-block');
+  imgEl.src = path;
+  return imgEl;
 }
 
-const createBlock = (newElement, classNameList, col) => {
+const resetBgBlock = () => {
+  const blockList = document.querySelectorAll('.block');
+  blockList.forEach((block) => {
+    block.style.backgroundColor = '';
+  });
+};
+
+const renderEvents = () => {
+  if (checkValue) {
+    eventsEl.style.display = 'flex';
+  }
+
+  eventsEl.innerHTML = '';
+  eventsList.forEach((event) => {
+    const eventEl = document.createElement('p');
+    eventEl.classList.add('event');
+    eventEl.innerHTML = event;
+    eventsEl.appendChild(eventEl);
+  });
+}
+
+
+
+const handleClick = (id, blockEl) => {
+  const [row, col] = id.split('-');
+  resetBgBlock();
+  blockEl.style.backgroundColor = 'white';
+
+  if (ticTacList[row][col] === 0) {
+    ticTacList[row][col] = player;
+
+    if (player === 'X') {
+      eventsList.push('Adicionou X');
+      blockEl.appendChild(createImage('../assets/x.png'));
+    } else {
+      eventsList.push('Adicionou O');
+      blockEl.appendChild(createImage('../assets/o.png'));
+    }
+
+    renderEvents();
+    player = player === 'X' ? 'O' : 'X';
+  }
+}
+
+const createBlock = (newElement, classNameList, id) => {
   const blockEl = document.createElement('div');
-  blockEl.addEventListener('click', () => handleClick(col));
-  blockEl.classList.add(...classNameList);
+  blockEl.classList.add(...classNameList, id);
+  blockEl.addEventListener('click', () => handleClick(id, blockEl));
   newElement.appendChild(blockEl);
 }
 
@@ -37,6 +89,18 @@ const createTicTac = () => {
   });
 }
 
+const handleCheck = () => {
+  checkEl.addEventListener('click', () => {
+    checkValue = checkEl.checked;
+    if (checkEl.checked && eventsList.length > 0) {
+      eventsEl.style.display = 'flex';
+    } else {
+      eventsEl.style.display = 'none';
+    }
+  })
+};
+
 window.onload = () => {
   createTicTac();
+  handleCheck();
 }
